ad36592ecfa569d00c0729e6f15e5a0b9d25b743
cinyearchan <cinyearchan@gmail.com>
2019-10-22 15:01:09 +0800

    index finish

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <div class="title">热销推荐</div>
-    <ul>
-      <li class="item border-bottom" v-for="item in recommendList" :key="item.id">
+    <ul v-if="showRecommend">
+      <li class="item border-bottom" v-for="item in list" :key="item.id">
         <div class="item-img-wrapper">
           <img :src="item.imgUrl" alt class="item-img" />
         </div>
@@ -19,46 +19,12 @@
 <script>
 export default {
   name: 'HomeRecommend',
-  data () {
-    return {
-      recommendList: [
-        {
-          id: '0001',
-          imgUrl: 'http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg',
-          title: '上海迪士尼乐园',
-          desc: '全球最大的迪士尼城堡'
-        },
-        {
-          id: '0002',
-          imgUrl: 'http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg',
-          title: '上海迪士尼乐园',
-          desc: '全球最大的迪士尼城堡'
-        },
-        {
-          id: '0003',
-          imgUrl: 'http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg',
-          title: '上海迪士尼乐园',
-          desc: '全球最大的迪士尼城堡'
-        },
-        {
-          id: '0004',
-          imgUrl: 'http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg',
-          title: '上海迪士尼乐园',
-          desc: '全球最大的迪士尼城堡'
-        },
-        {
-          id: '0005',
-          imgUrl: 'http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg',
-          title: '上海迪士尼乐园',
-          desc: '全球最大的迪士尼城堡'
-        },
-        {
-          id: '0006',
-          imgUrl: 'http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg',
-          title: '上海迪士尼乐园',
-          desc: '全球最大的迪士尼城堡'
-        }
-      ]
+  props: {
+    list: Array
+  },
+  computed: {
+    showRecommend () {
+      return this.list.length
     }
   }
 }
--- src/pages/home/components/Weekend.vue
+++ src/pages/home/components/Weekend.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <div class="title">周末去哪儿</div>
-    <ul>
-      <li class="item border-bottom" v-for="item in recommendList" :key="item.id">
+    <ul v-if="showWeekend">
+      <li class="item border-bottom" v-for="item in list" :key="item.id">
         <div class="item-img-wrapper">
           <img :src="item.imgUrl" alt class="item-img" />
         </div>
@@ -18,46 +18,12 @@
 <script>
 export default {
   name: 'HomeWeekend',
-  data () {
-    return {
-      recommendList: [
-        {
-          id: '0001',
-          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9f/f585152825459.jpg_r_640x214_5d46e4cc.jpg',
-          title: '上海迪士尼乐园',
-          desc: '全球最大的迪士尼城堡'
-        },
-        {
-          id: '0002',
-          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9f/f585152825459.jpg_r_640x214_5d46e4cc.jpg',
-          title: '上海迪士尼乐园',
-          desc: '全球最大的迪士尼城堡'
-        },
-        {
-          id: '0003',
-          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9f/f585152825459.jpg_r_640x214_5d46e4cc.jpg',
-          title: '上海迪士尼乐园',
-          desc: '全球最大的迪士尼城堡'
-        },
-        {
-          id: '0004',
-          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9f/f585152825459.jpg_r_640x214_5d46e4cc.jpg',
-          title: '上海迪士尼乐园',
-          desc: '全球最大的迪士尼城堡'
-        },
-        {
-          id: '0005',
-          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9f/f585152825459.jpg_r_640x214_5d46e4cc.jpg',
-          title: '上海迪士尼乐园',
-          desc: '全球最大的迪士尼城堡'
-        },
-        {
-          id: '0006',
-          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9f/f585152825459.jpg_r_640x214_5d46e4cc.jpg',
-          title: '上海迪士尼乐园',
-          desc: '全球最大的迪士尼城堡'
-        }
-      ]
+  props: {
+    list: Array
+  },
+  computed: {
+    showWeekend () {
+      return this.list.length
     }
   }
 }
@@ -67,7 +33,6 @@ export default {
 @import '~styles/Mixins.styl';
 
 .title {
-  margin-top: 0.2rem;
   line-height: 0.8rem;
   background: #eee;
   text-indent: 0.2rem;
@@ -76,7 +41,7 @@ export default {
 .item-img-wrapper {
   overflow: hidden;
   height: 0;
-  padding-bottom: 33.9%;
+  padding-bottom: 37.09%;
 
   .item-img {
     width: 100%;
